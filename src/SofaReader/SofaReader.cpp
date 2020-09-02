9846e6a4537b7261caefe3afdc38dc412f5158e9
tmtakashi <tmtakashi7@gmail.com>
2020-09-02 20:07:19 +0900

    added cmake settings

---
+++ src/SofaReader/SofaReader.cpp
@@ -0,0 +1,24 @@
+#include "SofaReader.hpp"
+#include <vector>
+
+SofaReader::SofaReader(std::string path) : sofaFile(path)
+{
+    assert(sofaFile.IsValid());
+
+    // set sample rate
+    const bool validSampleRate = sofaFile.GetSamplingRate(sampleRate);
+    assert(validSampleRate);
+
+    M = static_cast<unsigned int>(sofaFile.GetNumMeasurements());
+    R = static_cast<unsigned int>(sofaFile.GetNumReceivers());
+    N = static_cast<unsigned int>(sofaFile.GetNumDataSamples());
+    // check if the is for human ears
+    assert(R == 2);
+
+    // extract HRIR
+    std::vector<double> tmp;
+}
+
+SofaReader::~SofaReader()
+{
+}
