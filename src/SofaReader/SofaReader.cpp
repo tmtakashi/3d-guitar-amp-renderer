e051129633980b2860209351f52b461cb72ece8f
tmtakashi <tmtakashi7@gmail.com>
2020-09-03 08:25:38 +0900

    data sample extractor

--- src/SofaReader/SofaReader.cpp
+++ src/SofaReader/SofaReader.cpp
@@ -1,14 +1,25 @@
 #include "SofaReader.hpp"
 #include <vector>
 
-SofaReader::SofaReader(std::string_view path) : sofaFile(path)
+SofaReader::SofaReader(std::string_view path) : sofaFile(std::string(path))
+{
+    extractSampleRate();
+    extractHRIRPair();
+}
+
+SofaReader::~SofaReader()
+{
+}
+
+void SofaReader::extractSampleRate()
 {
     assert(sofaFile.IsValid());
-
-    // set sample rate
     const bool validSampleRate = sofaFile.GetSamplingRate(sampleRate);
     assert(validSampleRate);
+}
 
+void SofaReader::extractHRIRPair()
+{
     M = static_cast<unsigned int>(sofaFile.GetNumMeasurements());
     R = static_cast<unsigned int>(sofaFile.GetNumReceivers());
     N = static_cast<unsigned int>(sofaFile.GetNumDataSamples());
@@ -18,8 +29,40 @@ SofaReader::SofaReader(std::string_view path) : sofaFile(path)
     // extract HRIR
     std::vector<double> tmp;
     sofaFile.GetDataIR(tmp);
+
+    // resize HRIR containers
+    hrirL.resize(M, N);
+    hrirR.resize(M, N);
+    for (std::size_t receiverIdx = 0; receiverIdx < R; receiverIdx++)
+    {
+        for (std::size_t measurementIdx = 0; measurementIdx < M; receiverIdx++)
+        {
+            for (std::size_t numSampleIdx = 0; numSampleIdx < N; numSampleIdx++)
+            {
+                const std::size_t index = array3DIndex(measurementIdx, receiverIdx, numSampleIdx, M, R, N);
+                switch (receiverIdx)
+                {
+                case 0:
+                    hrirL(measurementIdx, numSampleIdx) = tmp[index];
+                    break;
+                case 1:
+                    hrirR(measurementIdx, numSampleIdx) = tmp[index];
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
 }
 
-SofaReader::~SofaReader()
+std::size_t
+SofaReader::array3DIndex(const unsigned long i,
+                         const unsigned long j,
+                         const unsigned long k,
+                         const unsigned long dim1,
+                         const unsigned long dim2,
+                         const unsigned long dim3) noexcept
 {
+    return dim2 * dim3 * i + dim3 * j + k;
 }
