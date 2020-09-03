b1ad290a79be503ee88720b18435f8b913ea0bde
tmtakashi <tmtakashi7@gmail.com>
2020-09-03 17:45:36 +0900

    HRIR grid extraction

--- src/SofaReader/SofaReader.cpp
+++ src/SofaReader/SofaReader.cpp
@@ -1,10 +1,17 @@
 #include "SofaReader.hpp"
 #include <vector>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
 
-SofaReader::SofaReader(std::string_view path) : sofaFile(std::string(path))
+SofaReader::SofaReader(std::string_view path)
+    : hrirFile(std::string(path)), sofaFile(std::string(path))
 {
+    assert(hrirFile.IsValid());
+    assert(sofaFile.IsValid());
     extractSampleRate();
     extractHRIRPair();
+    extractHRIRGrid();
 }
 
 SofaReader::~SofaReader()
@@ -13,33 +20,32 @@ SofaReader::~SofaReader()
 
 void SofaReader::extractSampleRate()
 {
-    assert(sofaFile.IsValid());
-    const bool validSampleRate = sofaFile.GetSamplingRate(sampleRate);
+    const bool validSampleRate = hrirFile.GetSamplingRate(sampleRate);
     assert(validSampleRate);
 }
 
 void SofaReader::extractHRIRPair()
 {
-    M = static_cast<unsigned int>(sofaFile.GetNumMeasurements());
-    R = static_cast<unsigned int>(sofaFile.GetNumReceivers());
-    N = static_cast<unsigned int>(sofaFile.GetNumDataSamples());
+    M = static_cast<unsigned int>(hrirFile.GetNumMeasurements());
+    R = static_cast<unsigned int>(hrirFile.GetNumReceivers());
+    N = static_cast<unsigned int>(hrirFile.GetNumDataSamples());
     // check if the is for human ears
     assert(R == 2);
 
     // extract HRIR
     std::vector<double> tmp;
-    sofaFile.GetDataIR(tmp);
+    hrirFile.GetDataIR(tmp);
 
     // resize HRIR containers
     hrirL.resize(M, N);
     hrirR.resize(M, N);
     for (std::size_t receiverIdx = 0; receiverIdx < R; receiverIdx++)
     {
-        for (std::size_t measurementIdx = 0; measurementIdx < M; receiverIdx++)
+        for (std::size_t measurementIdx = 0; measurementIdx < M; measurementIdx++)
         {
             for (std::size_t numSampleIdx = 0; numSampleIdx < N; numSampleIdx++)
             {
-                const std::size_t index = array3DIndex(measurementIdx, receiverIdx, numSampleIdx, M, R, N);
+                std::size_t index = array3DIndex(measurementIdx, receiverIdx, numSampleIdx, M, R, N);
                 switch (receiverIdx)
                 {
                 case 0:
@@ -56,13 +62,44 @@ void SofaReader::extractHRIRPair()
     }
 }
 
-std::size_t
-SofaReader::array3DIndex(const unsigned long i,
-                         const unsigned long j,
-                         const unsigned long k,
-                         const unsigned long dim1,
-                         const unsigned long dim2,
-                         const unsigned long dim3) noexcept
+void SofaReader::extractHRIRGrid()
 {
-    return dim2 * dim3 * i + dim3 * j + k;
+    sofa::Coordinates::Type coordinates;
+    sofa::Units::Type units;
+    const bool ok = sofaFile.GetSourcePosition(coordinates, units);
+    assert(ok);
+    // check if the coordinate is spherical
+    assert(coordinates == sofa::Coordinates::Type::kSpherical);
+    // check if the units are in degree, degree, metre
+    assert(units == sofa::Units::Type::kSphericalUnits);
+
+    std::vector<std::size_t> dims;
+    sofaFile.GetVariableDimensions(dims, "SourcePosition");
+    // check if the dimention is {measurements, coords}
+    assert(dims.size() == 2);
+
+    // temporary position container
+    std::vector<double> pos(dims[0] * dims[1]);
+    sofaFile.GetSourcePosition(&pos[0], dims[0], dims[1]);
+
+    hrirGrid.resize(dims[0], dims[1]);
+    for (std::size_t i = 0; i < dims[0]; i++)
+    {
+        for (std::size_t j = 0; j < dims[1]; j++)
+        {
+            const std::size_t index = array2DIndex(i, j, dims[0], dims[1]);
+            hrirGrid(i, j) = pos[index];
+        }
+    }
+    // convert degree to radian
+    degToRadMat(hrirGrid);
+}
+
+void SofaReader::degToRadMat(Eigen::MatrixXd &degSphCoords)
+{
+    for (std::size_t i = 0; i != degSphCoords.rows(); ++i)
+    {
+        degSphCoords.row(i)(0) = degreesToRadians(degSphCoords.row(i)(0));
+        degSphCoords.row(i)(1) = degreesToRadians(degSphCoords.row(i)(1));
+    }
 }
--- src/SofaReader/SofaReader.hpp
+++ src/SofaReader/SofaReader.hpp
@@ -6,7 +6,8 @@
 class SofaReader
 {
 private:
-    sofa::SimpleFreeFieldHRIR sofaFile;
+    sofa::SimpleFreeFieldHRIR hrirFile;
+    sofa::File sofaFile;
     double sampleRate;
     // number of measurements
     unsigned int M;
@@ -18,16 +19,30 @@ private:
     // HRIR L, R pair
     Eigen::MatrixXd hrirL;
     Eigen::MatrixXd hrirR;
+    // Grid Positions
+    Eigen::MatrixXd hrirGrid;
 
     void extractSampleRate();
     void extractHRIRPair();
+    void extractHRIRGrid();
 
+    static void degToRadMat(Eigen::MatrixXd &degSphCoords);
     static std::size_t array3DIndex(const unsigned long i,
                                     const unsigned long j,
                                     const unsigned long k,
                                     const unsigned long dim1,
                                     const unsigned long dim2,
-                                    const unsigned long dim3) noexcept;
+                                    const unsigned long dim3) noexcept
+    {
+        return dim2 * dim3 * i + dim3 * j + k;
+    }
+    static std::size_t array2DIndex(const unsigned long i,
+                                    const unsigned long j,
+                                    const unsigned long dim1,
+                                    const unsigned long dim2) noexcept
+    {
+        return dim2 * i + j;
+    }
 
 public:
     explicit SofaReader(std::string_view path);
