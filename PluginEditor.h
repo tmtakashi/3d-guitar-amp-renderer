8f676d4f005d3e8c0b2a13ec45dc4f19501a56e7
tmtakashi <tmtakashi7@gmail.com>
2020-09-04 13:14:47 +0900

    確認用に簡単なスライダー追加

--- PluginEditor.h
+++ PluginEditor.h
@@ -3,20 +3,29 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor,
+                                        public juce::Slider::Listener
 {
 public:
-    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+    explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
     ~AudioPluginAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics &) override;
     void resized() override;
 
-private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    AudioPluginAudioProcessor& processorRef;
+    void sliderValueChanged(juce::Slider *slider) override
+    {
+        if (slider == &testSlider)
+            value = testSlider.getValue();
+    }
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+private:
+    juce::Slider testSlider;
+    juce::Label testLabel;
+
+    float value;
+    AudioPluginAudioProcessor &processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
