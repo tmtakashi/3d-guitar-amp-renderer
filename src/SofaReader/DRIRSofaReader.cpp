7122367222632f7230929470c56d814d7230a51f
tmtakashi <tmtakashi7@gmail.com>
2020-09-06 17:02:46 +0900

    ir読み込み処理追加

--- src/SofaReader/DRIRSofaReader.cpp
+++ src/SofaReader/DRIRSofaReader.cpp
@@ -17,14 +17,13 @@ void DRIRSofaReader::extractIRs()
     irFile.GetDataIR(tmp);
     // resize DRIR container
     drirs.resize(getR(), getN());
-    for (std::size_t receiverIdx = 0; receiverIdx < getR(); receiverIdx++)
+    for (std::size_t measurementIdx = 0; measurementIdx < getM(); measurementIdx++)
     {
-        for (std::size_t measurementIdx = 0; measurementIdx < getM(); measurementIdx++)
+        for (std::size_t receiverIdx = 0; receiverIdx < getR(); receiverIdx++)
         {
             for (std::size_t numSampleIdx = 0; numSampleIdx < getN(); numSampleIdx++)
             {
                 std::size_t index = array3DIndex(measurementIdx, receiverIdx, numSampleIdx, getM(), getR(), getN());
-                std::cout << tmp[index] << std::endl;
                 drirs(receiverIdx, numSampleIdx) = tmp[index];
             }
         }
