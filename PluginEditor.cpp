77dbde04f79f33171721eda787505ccc8c0b48c9
kzmwtnb <63474381+kzmwtnb@users.noreply.github.com>
2020-09-06 23:05:31 +0900

    getWidth()でエラーが，，，

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -8,17 +8,54 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(AudioPluginAudi
     juce::ignoreUnused(processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize(400, 300);
+
+    setSize(600, 400);
+
+    // setup ComboBox
+    addAndMakeVisible(textLabel);
+    textLabel.setText("Select the azimuth.", juce::dontSendNotification);
+    textLabel.setFont(textFont);
+
+    // add items to the combo-box
+    // addAndMakeVisible(styleMenu);
+    // styleMenu.addItem("0", 1);
+    // styleMenu.addItem("+45", 2);
+    // styleMenu.addItem("+90", 3);
+    // styleMenu.addItem("-90", 4);
+    // styleMenu.addItem("-45", 5);
+    // styleMenu.onChange = [this] { styleMenuChanged(); };
+    // styleMenu.setSelectedId(1);
 
     // setup slider
-    addAndMakeVisible(testSlider);
-    testSlider.setRange(0, 100);
-    testSlider.addListener(this);
+    addAndMakeVisible(azimuthDial);
+    azimuthDial.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalDrag);
+    // azimuthDial.setRotaryParameters();
+    azimuthDial.setRange(0, 360);
+    azimuthDial.addListener(this);
 
     // setup slider label
     addAndMakeVisible(testLabel);
     testLabel.setText("Value", juce::dontSendNotification);
-    testLabel.attachToComponent(&testSlider, true);
+    testLabel.attachToComponent(&azimuthDial, true);
+
+    // set up file loader
+    fileComp.reset(new juce::FilenameComponent("fileComp",
+                                               {},                      // current file
+                                               false,                   // can edit file name,
+                                               false,                   // is directory,
+                                               false,                   // is for saving,
+                                               {},                      // browser wildcard suffix,
+                                               {},                      // enforced suffix,
+                                               "Select file to open")); // text when nothing selected
+    
+    addAndMakeVisible(fileComp.get());
+    fileComp->addListener(this);
+
+    textContent.reset(new juce::TextEditor());
+    addAndMakeVisible(textContent.get());
+    textContent->setMultiLine(true);
+    textContent->setReadOnly(true);
+    textContent->setCaretVisible(false);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -33,11 +70,18 @@ void AudioPluginAudioProcessorEditor::paint(juce::Graphics &g)
 
     g.setColour(juce::Colours::white);
     g.setFont(15.0f);
-    g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    // g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
     auto sliderLeft = 120;
-    testSlider.setBounds(sliderLeft, 20, getWidth() - sliderLeft - 10, 20);
+    
+    azimuthDial.setBounds(sliderLeft, 50, getWidth() - sliderLeft - 10, 50);
+    textLabel.setBounds(sliderLeft, 100, getWidth() - sliderLeft - 10, 200);
+    // styleMenu.setBounds(sliderLeft, 250, getWidth() - sliderLeft, 20);
+
+    fileComp->setBounds    (sliderLeft, 300, getWidth() - sliderLeft - 20, 20);
+    textContent->setBounds (sliderLeft, 350, getWidth() - sliderLeft - 20,  20);
 }
--- PluginEditor.h
+++ PluginEditor.h
@@ -4,7 +4,8 @@
 
 //==============================================================================
 class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor,
-                                        public juce::Slider::Listener
+                                        public juce::Slider::Listener,
+                                        public juce::FilenameComponentListener
 {
 public:
     explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
@@ -16,16 +17,60 @@ public:
 
     void sliderValueChanged(juce::Slider *slider) override
     {
-        if (slider == &testSlider)
-            value = testSlider.getValue();
+        if (slider == &azimuthDial)
+            azimuth = azimuthDial.getValue();
+    }
+    // void styleMenuChanged()
+    // {
+    //     switch(styleMenu.getSelectedId())
+    //     {
+    //         case 1:
+    //             textFont.setStyleFlags(juce::Font::plain);
+    //             break;
+    //         case 2:
+    //             textFont.setStyleFlags(juce::Font::bold);
+    //             break;
+    //         case 3:
+    //             textFont.setStyleFlags(juce::Font::italic);
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     textLabel.setFont(textFont);
+    // }
+
+    void filenameComponentChanged(juce::FilenameComponent *fileComponentThatHasChanged) override
+    {
+        if (fileComponentThatHasChanged == fileComp.get())
+            readFile(fileComp->getCurrentFile());
+    }
+
+    void readFile(const juce::File &fileToRead)
+    {
+        if (!fileToRead.existsAsFile()) // [1]
+            return;
+
+        auto fileText = fileToRead.loadFileAsString();
+
+        textContent->setText(fileText);
     }
 
 private:
-    juce::Slider testSlider;
+
+    juce::Slider azimuthDial;
     juce::Label testLabel;
 
-    float value;
+    float azimuth;
     AudioPluginAudioProcessor &processorRef;
+
+    juce::Label textLabel;
+    juce::Font textFont{14.0f};
+    juce::ComboBox styleMenu;
+
+    // File reading
+    std::unique_ptr<juce::FilenameComponent> fileComp;
+    std::unique_ptr<juce::TextEditor> textContent;
+    // juce::File path(File("/SofaReader").getChildFile("to").getChildFile("file.txt"));
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
