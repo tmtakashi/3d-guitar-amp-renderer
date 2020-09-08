37b36fe2ce98f4455682fd4dd92130b043135470
tmtakashi <tmtakashi7@gmail.com>
2020-09-08 18:57:40 +0900

    added radial filter generation function

--- src/BinauralRenderer/SphUtils.cpp
+++ src/BinauralRenderer/SphUtils.cpp
@@ -1,6 +1,11 @@
 #include "SphUtils.hpp"
+#include "boost/math/special_functions/bessel.hpp"
 #include "boost/math/special_functions/spherical_harmonic.hpp"
 #include <cmath>
+
+constexpr const double rho = 1.293;      // density of air
+constexpr const double kappa = 142.0e3;  // bulk modulus
+const double c = std::sqrt(kappa / rho); // speed of sound
 
 void getSphHarmMtx(Eigen::MatrixXcd &sphHarmMtx,
                    Eigen::MatrixXd &positionGridMtx, unsigned int order) {
@@ -28,4 +33,36 @@ void SFT(Eigen::VectorXcd &freqDomainSignals, Eigen::MatrixXd &positionGrid,
   /* calculate spherical fourier transfrom using pseudo inverse */
   sphCoeffs = Y.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV)
                   .solve(freqDomainSignals);
+}
+
+double bn(unsigned int n, double k, double radius,
+          MicArrayConfig micArrayConfig) {
+  if (micArrayConfig == MicArrayConfig{ArrayType::open, MicType::omni}) {
+    return boost::math::sph_bessel(n, k * radius);
+  }
+  /* TODO: return bns for other configs */
+  return boost::math::sph_bessel(n, k * radius);
+}
+
+void getRadialFilter(Eigen::MatrixXcd &radFiltMtx, double radius,
+                     unsigned int nfft, double fs, unsigned int order,
+                     MicArrayConfig micArrayConfig) {
+  assert(radFiltMtx.rows() == std::floor(nfft / 2) + 1);
+  assert(radFiltMtx.cols() == (order + 1) * (order + 1));
+  Eigen::VectorXd freqs =
+      Eigen::VectorXd::LinSpaced(std::floor(nfft / 2) + 1, 0, floor(fs / 2));
+  for (std::size_t i = 0; i < freqs.size(); i++) {
+    double k = 2 * M_PI * freqs(i) / c;
+    for (int n = 0; n < order; n++) {
+      for (int m = 0; m < 2 * n + 1; m++) {
+        auto b = bn(n, k, radius, micArrayConfig);
+        if (b < 1e-12) {
+          b = 1e-12; // avoid zero division
+                     // TODO: define zero division avoidance for complex bn
+        }
+        radFiltMtx(i, n * n + m) =
+            1.0 / (4 * M_PI * std::pow(std::complex<double>(0, 1), 2) * b);
+      }
+    }
+  }
 }
--- src/BinauralRenderer/SphUtils.hpp
+++ src/BinauralRenderer/SphUtils.hpp
@@ -1,7 +1,41 @@
 #include <Eigen/Dense>
+#include <complex>
+
+/* mic array configuration */
+enum class ArrayType { open, rigid, dual };
+
+enum class MicType {
+  omni,
+  cardioid,
+};
+
+struct MicArrayConfig {
+  ArrayType arrayType;
+  MicType micType;
+
+  bool operator==(const MicArrayConfig &rhs) {
+    return arrayType == rhs.arrayType && micType == rhs.micType;
+  }
+};
 
 void getSphHarmMtx(Eigen::MatrixXcd &sphHarmMtx,
                    Eigen::MatrixXd &positionGridMtx, unsigned int order);
 
 void SFT(Eigen::VectorXcd &freqDomainSignals, Eigen::MatrixXd &positionGrid,
          Eigen::VectorXcd &sphCoeffs, unsigned int order);
+
+/**
+ * calculates radial filter in frequency domain (currently only open sphere
+ *only)
+ * @return radial filter vector (nFreqs, (order + 1)^2)
+ **/
+void getRadialFilter(Eigen::MatrixXcd &radFiltMtx, double radius,
+                     unsigned int nfft, double fs, unsigned int order,
+                     MicArrayConfig config);
+
+/**
+ * radial component of spherical harmonic expantion
+ **/
+std::complex<double> bn(int n, double k, double r,
+                        MicArrayConfig micArrayConfig, double alpha,
+                        double radius);
