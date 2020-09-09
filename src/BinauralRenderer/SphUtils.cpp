288ce52a141235283255970abf61455d6f694883
tmtakashi <tmtakashi7@gmail.com>
2020-09-09 18:46:00 +0900

    matrix row permutation function

--- src/BinauralRenderer/SphUtils.cpp
+++ src/BinauralRenderer/SphUtils.cpp
@@ -88,8 +88,8 @@ void getRadialFilter(Eigen::MatrixXcd &radFiltMtx, double radius,
   }
 }
 
-std::vector<unsigned> getReversedMnIds(unsigned order) {
-  std::vector<unsigned> reversedMnIds;
+std::vector<std::size_t> getReversedMnIds(unsigned order) {
+  std::vector<std::size_t> reversedMnIds;
   for (unsigned n = 0; n < order + 1; n++) {
     for (unsigned m = 0; m < 2 * n + 1; m++) {
       reversedMnIds.push_back((n + 1) * (n + 1) - m - 1);
